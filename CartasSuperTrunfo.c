357a5f9c62db565204fb4ec713018c90ec061dc2
taypannn <taypan.contato@gmail.com>
2025-04-30 08:04:26 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,44 @@
 //Teste larissa
 
 int main() {
+
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos_turisticos1;
+
+    
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos_turisticos2;
+
+
+    printf("populacao1:\n");
+    printf("pib1:\n");
+    printf("area1:\n");
+    printf("pontos_turisticos1:\n"):
+
+
+    scanf("populacao1:\n");
+    scanf("pib1:\n");
+    scanf("area1:\n");
+    scanf("pontos_turisticos1:\n");
+
+    scanf("populacao2:\n");
+    scanf("pib2:\n");
+    scanf("area2:\n");
+    scanf("pontos_turisticos2:\n");
+    
+    
+
+    
+
+
+
+
+
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
